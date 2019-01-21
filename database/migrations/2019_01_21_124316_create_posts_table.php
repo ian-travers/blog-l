75ca9d3f76e9268a0b049996a95786c0264c6041
Ian Travers <mr.itpe@gmail.com>
2019-01-21 16:57:54 +0300

    Create Post model with migration

---
+++ database/migrations/2019_01_21_124316_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('author_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt');
+            $table->text('body');
+            $table->string('image')->nullable();
+            $table->timestamps();
+
+            $table->foreign('author_id')
+                ->references('id')->on('users')
+                ->onDelete('restrict');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
